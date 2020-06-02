8b53fa00889bd3378ed6b404c0e8d5160326db4c
José Pedro <63018518+JosePedro-22@users.noreply.github.com>
2020-06-02 18:39:42 -0300

    update_projeto_vs2.00

--- proj.c
+++ proj.c
@@ -4,10 +4,23 @@
 #include <time.h>
 
 typedef struct{
-	char localPartida[100];
-	char localDestino[100];
+	int localPartida;
+	int localDestino;
 	int dias_de_Estadia;
 } Projeto;
+
+void LPD(){
+	printf("1. SAO LUIS\n");
+	printf("2. SAO PAULO\n");
+	printf("3. RECIFE\n");
+	printf("4. SALVADOR\n");
+	printf("5. RIO DE JANEIRO\n");
+	printf("6. GRAMADO\n");
+	printf("7. OURO PRETO\n");
+	printf("8. FORTALEZA\n");
+	printf("9. MACEIO\n");
+	printf("10. BRASILIA\n");
+}
 
 int random_max_min (int y){
 
@@ -15,6 +28,7 @@ int random_max_min (int y){
     	if(rand()%y > 200) return rand()%y;
     }while(rand()%y < 200);
 }
+
 void vazio(){
 	printf("             ");
 }
@@ -24,7 +38,6 @@ void vazio_1(){
 }
 
 void display_op(int x){
-
 	vazio_1();
 	printf("Companhias Aereas Disponiveis\n");
 	printf("\n");
@@ -42,39 +55,38 @@ void display_op(int x){
 	printf("Codigo 828. Azul R$ %d.00\n", random_max_min(330)*x);
 	vazio_1();
 	printf("Codigo 951. LATAM R$ %d.00\n", random_max_min(450)*x);
-
 }
 
 void switch_case(){
 	int num;
 
-	printf("Ola digite o codigo da Companhia para adquirir sua passagem!\n");
+	printf("\n\nOla, digite o codigo da Companhia para adquirir sua passagem!\n\n");
 	vazio();
 	scanf("%d", &num);
 
 		switch(num){
-			case 423:vazio_1(); printf("Passagem comprada, obrigado e volte sempre.\n");
+			case 423:vazio_1(); printf("\n\n\n          Passagem comprada, obrigado e volte sempre.\n\n");
 				break; 
-			case 721:vazio_1(); printf("Passagem comprada, obrigado e volte sempre.\n");
+			case 721:vazio_1(); printf("\n\n\n          Passagem comprada, obrigado e volte sempre.\n\n");
 				break;
-			case 836:vazio_1(); printf("Passagem comprada, obrigado e volte sempre.\n");
+			case 836:vazio_1(); printf("\n\n\n          Passagem comprada, obrigado e volte sempre.\n\n");
 				break;
-			case 041:vazio_1(); printf("Passagem comprada, obrigado e volte sempre.\n");
+			case 041:vazio_1(); printf("\n\n\n          Passagem comprada, obrigado e volte sempre.\n\n");
 				break;
-			case 524:vazio_1(); printf("Passagem comprada, obrigado e volte sempre.\n");
+			case 524:vazio_1(); printf("\n\n\n          Passagem comprada, obrigado e volte sempre.\n\n");
 				break;
-			case 828:vazio_1(); printf("Passagem comprada, obrigado e volte sempre.\n");
+			case 828:vazio_1(); printf("\n\n\n          Passagem comprada, obrigado e volte sempre.\n\n");
 				break;
-			case 951:vazio_1(); printf("Passagem comprada, obrigado e volte sempre.\n");
+			case 951:vazio_1(); printf("\n\n\n          Passagem comprada, obrigado e volte sempre.\n\n");
 				break;
-			default: printf("Desculpe sua opcao de Companhia Aerea indisponivel!\n");
+			default: printf("\n\n\n\n        Desculpe sua opcao de Companhia Aerea indisponivel!\n");
 		}
 }
 
 void fundo(){
 	for (int i = 0; i < 6; ++i)
 	{
-		printf("*******************************************************\n");
+		printf("******************************************************************\n");
 	}
 }
 
@@ -107,6 +119,7 @@ void tela_1()
 	printf("                      Sim? s\n");
 	printf("                      Nao? n\n");
 }
+
 void tela_2(){                           
     printf("       '##::::'##:'########:'##::: ##:'##::::'##:         \n");
     printf("        ###::'###: ##.....:: ###:: ##: ##:::: ##:         \n");
@@ -119,17 +132,41 @@ void tela_2(){
     printf("                                                          \n");
 	printf("		1. Comprar sua Passagen\n");
 	printf("		2. Exit\n");
-	//printf("menu 1. comprar 2. exit\n");
+}
+
+void cabecalho(){
+	printf("   =========================================================================\n");
+	printf("   =========================================================================\n");
+	printf("   =================SISTEMA DE COMPRA DE PASSAGEN AERIAS====================\n");
+	printf("   =========================================================================\n");
+	printf("   =========================================================================\n");
+	printf("   =========================================================================\n");
+	printf("   =========================================================================\n");
+	printf("   =========================================================================\n");
+	printf("   =====================FACULDADE PITAGORAS - SAO LUIS======================\n");
+	printf("   ======================CURSO: CIENCIA DA COMPUTACAO=======================\n");
+	printf("   ====================AUTOR: JOSE PEDRO SOUZA MESQUITA=====================\n");
+	printf("   =========================================================================\n");
+	printf("   =========================================================================\n");
+	
+}
+void rodape(){
+	printf("\n\nCreditos: Jose Pedro Souza Mesquita - Todos os direitos reservado.\n");
+	printf("                https://github.com/JosePedro-22\n");
+	printf("                 josepedromesquita@hotmail.com\n");
 }
 int main(){
 	char caracter;
 	int op;
 	Projeto chamada;
 
+	cabecalho();
 	tela_1();
 	vazio();
 	vazio();
 	scanf("%s", &caracter);
+	fflush(stdin);
+
 	if(caracter == 's'||'S'){
 		system("cls");
 
@@ -137,33 +174,40 @@ int main(){
 		vazio();
 		vazio_1();
 		scanf("%d", &op);
+		fflush(stdin);
 		system("cls");
 		if(op == 1)
 		{
+			
 			fundo();
 			vazio();
-			printf("Digite o seu Local de Partida\n");
-			vazio();
-			gets(chamada.localPartida);
-			/*fundo();
-			system("cls");
-
-			fundo();
-			vazio();
-			printf("Digite o seu Local de Destino\n");
-			vazio();
-			gets(chamada.localDestino);
+			printf("De onde voce quer partir ?\n");
+			LPD();
+			printf("Digite o codigo do Local de Partida\n");
+			scanf("%d", &chamada.localPartida);
+			fflush(stdin);
 			fundo();
 			system("cls");
-
+			
+			fundo();
+			vazio();
+			printf("Para onde voce quer chegar ?\n");
+			LPD();
+			printf("Digite o codigo do Local de Destino\n");
+			scanf("%d", &chamada.localDestino);
+			fflush(stdin);
+			fundo();
+			system("cls");
+			
 			fundo();
 			vazio();
 			printf("Dias de Estadia\n");
 			vazio();
 			scanf("%d", &chamada.dias_de_Estadia);
+			fflush(stdin);
 			fundo();
 			system("cls");
-
+			
 			fundo();
 			display_op(chamada.dias_de_Estadia/2);
 			fundo();
@@ -171,20 +215,21 @@ int main(){
 
 			switch_case();
 			fundo();
-			*/
+			printf("\n\n");
+			rodape();
+			
 		}
 		else if(caracter == 'n'||'N')
 		{
-			//printf("ok\n");
-			//exit();
+			printf("\n\n");
+			rodape();
 			return 0;
 		}
+
 	}
-	else return 0;
+	else{
+		printf("\n\n");
+		rodape();
+		return 0;
+	}
 }
-//http://www.network-science.de/ascii/
-/*
-
-
-                                                    
-*/
